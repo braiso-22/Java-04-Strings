4517b9a4b56694a4e5cf7dc1751a314b5457f684
Brais <81094589+braiso-22@users.noreply.github.com>
2021-12-24 13:35:00 +0100

    Ej13 empezado

---
+++ Java-04-Strings/src/Strings/Ej13.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Strings;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brais
+ */
+public class Ej13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String numFin = "", hexa;
+        int numero, resto;
+        System.out.println("Introduce un numero en decimal y lo paso"
+                + " a hexadecimal");
+        numero = teclado.nextInt();
+
+        if (numero < 16) {
+            switch (numero) {
+                case 10:
+                    hexa = "A";
+                    break;
+                case 11:
+                    hexa = "B";
+                    break;
+                case 12:
+                    hexa = "C";
+                    break;
+                case 13:
+                    hexa = "D";
+                    break;
+                case 14:
+                    hexa = "E";
+                    break;
+                case 15:
+                    hexa = "F";
+                    break;
+                default:
+                    hexa = Integer.toString(numero);
+            }
+            numFin = hexa;
+        } else {
+
+            while (numero >= 16) {
+                resto = numero % 16;
+                switch (resto) {
+                    case 10:
+                        hexa = "A";
+                        break;
+                    case 11:
+                        hexa = "B";
+                        break;
+                    case 12:
+                        hexa = "C";
+                        break;
+                    case 13:
+                        hexa = "D";
+                        break;
+                    case 14:
+                        hexa = "E";
+                        break;
+                    case 15:
+                        hexa = "F";
+                        break;
+                    default:
+                        hexa = Integer.toString(resto);
+                }
+                numFin = hexa + numFin;
+                numero /= 16;
+
+            }
+            numFin=numero+numFin;
+            
+
+        }
+
+        System.out.println(numFin);
+    }
+
+}
