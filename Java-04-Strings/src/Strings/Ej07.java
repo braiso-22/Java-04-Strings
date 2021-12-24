32b6b431b464d7fd9614da0446cf16c605cbf875
Brais <81094589+braiso-22@users.noreply.github.com>
2021-12-24 12:33:29 +0100

    Ej07 terminado

--- Java-04-Strings/src/Strings/Ej07.java
+++ Java-04-Strings/src/Strings/Ej07.java
@@ -5,6 +5,8 @@
  */
 package Strings;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Brais
@@ -15,7 +17,30 @@ public class Ej07 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        String contraseña,
+                abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                hash = "FGHIJKLMNOPQRSTUVWXYZABCDE",
+                cifrada = "";
+        
+        System.out.println("Introduce una contraseña en mayusculas y la cifro:");
+        contraseña = teclado.nextLine();
+
+        for (int i = 0; i <= contraseña.length() - 1; i++) {
+            /*
+            for (int j = 0; j < 26; j++) {
+                if (contraseña.charAt(i) == abecedario.charAt(j)) {
+                    cifrada = cifrada + hash.charAt(j);
+                }
+            }*/
+            if (Character.isUpperCase(contraseña.charAt(i))) {
+                cifrada += hash.charAt(abecedario.indexOf(contraseña.charAt(i)));
+            } else {
+                cifrada += contraseña.charAt(i);
+            }
+
+        }
+        System.out.println("Tu contraseña cifrada es: " + cifrada);
     }
-    
+
 }
