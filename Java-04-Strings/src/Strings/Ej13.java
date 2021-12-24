55a874f5c44f82fb53925d01e8dc38320841d41e
Brais <81094589+braiso-22@users.noreply.github.com>
2021-12-24 14:05:03 +0100

    Ej13 terminado

--- Java-04-Strings/src/Strings/Ej13.java
+++ Java-04-Strings/src/Strings/Ej13.java
@@ -78,8 +78,29 @@ public class Ej13 {
                 numero /= 16;
 
             }
-            numFin=numero+numFin;
-            
+            switch (numero) {
+                case 10:
+                    hexa = "A";
+                    break;
+                case 11:
+                    hexa = "B";
+                    break;
+                case 12:
+                    hexa = "C";
+                    break;
+                case 13:
+                    hexa = "D";
+                    break;
+                case 14:
+                    hexa = "E";
+                    break;
+                case 15:
+                    hexa = "F";
+                    break;
+                default:
+                    hexa = Integer.toString(numero);
+            }
+            numFin = hexa + numFin;
 
         }
 
